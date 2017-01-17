d3a74e582e318ea62f9ee2dae84250e37ca08921
KOMPJULIANA\Julian <juleklejk@gmail.com>
2017-01-17 21:09:03 +0100

    fdfd

---
+++ FormDodajKontahenta.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace AplikacjaInzynierska
+{
+    partial class FormDodajKontrahenta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFDKImieKontrahenta = new System.Windows.Forms.TextBox();
+            this.textBoxFDKNazwiskoKontahenta = new System.Windows.Forms.TextBox();
+            this.textBoxFDKNazwaFirmy = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBoxFDKAdresEmail = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBoxFDKNip = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBoxFDKRegon = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.butFDKDodaj = new System.Windows.Forms.Button();
+            this.datadoINZDataSet1 = new AplikacjaInzynierska.DatadoINZDataSet();
+            this.textBoxFDKNumerTelefonu = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.datadoINZDataSet1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBoxFDKImieKontrahenta
+            // 
+            this.textBoxFDKImieKontrahenta.Location = new System.Drawing.Point(12, 35);
+            this.textBoxFDKImieKontrahenta.Name = "textBoxFDKImieKontrahenta";
+            this.textBoxFDKImieKontrahenta.Size = new System.Drawing.Size(111, 20);
+            this.textBoxFDKImieKontrahenta.TabIndex = 2;
+            // 
+            // textBoxFDKNazwiskoKontahenta
+            // 
+            this.textBoxFDKNazwiskoKontahenta.Location = new System.Drawing.Point(132, 35);
+            this.textBoxFDKNazwiskoKontahenta.Name = "textBoxFDKNazwiskoKontahenta";
+            this.textBoxFDKNazwiskoKontahenta.Size = new System.Drawing.Size(126, 20);
+            this.textBoxFDKNazwiskoKontahenta.TabIndex = 3;
+            // 
+            // textBoxFDKNazwaFirmy
+            // 
+            this.textBoxFDKNazwaFirmy.Location = new System.Drawing.Point(264, 35);
+            this.textBoxFDKNazwaFirmy.Name = "textBoxFDKNazwaFirmy";
+            this.textBoxFDKNazwaFirmy.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFDKNazwaFirmy.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(111, 23);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Imię Kontahenta";
+            // 
+            // label3
+            // 
+            this.label3.Location = new System.Drawing.Point(129, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Nazwisko Kontahenta";
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(264, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(100, 23);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Nazwa Firmy";
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(370, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(175, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Adres Email";
+            // 
+            // textBoxFDKAdresEmail
+            // 
+            this.textBoxFDKAdresEmail.Location = new System.Drawing.Point(370, 35);
+            this.textBoxFDKAdresEmail.Name = "textBoxFDKAdresEmail";
+            this.textBoxFDKAdresEmail.Size = new System.Drawing.Size(175, 20);
+            this.textBoxFDKAdresEmail.TabIndex = 8;
+            // 
+            // label6
+            // 
+            this.label6.Location = new System.Drawing.Point(261, 79);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "NIP";
+            // 
+            // textBoxFDKNip
+            // 
+            this.textBoxFDKNip.Location = new System.Drawing.Point(264, 105);
+            this.textBoxFDKNip.Name = "textBoxFDKNip";
+            this.textBoxFDKNip.Size = new System.Drawing.Size(100, 20);
+            this.textBoxFDKNip.TabIndex = 10;
+            // 
+            // label7
+            // 
+            this.label7.Location = new System.Drawing.Point(12, 79);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(111, 23);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "REGON";
+            // 
+            // textBoxFDKRegon
+            // 
+            this.textBoxFDKRegon.Location = new System.Drawing.Point(12, 105);
+            this.textBoxFDKRegon.Name = "textBoxFDKRegon";
+            this.textBoxFDKRegon.Size = new System.Drawing.Size(111, 20);
+            this.textBoxFDKRegon.TabIndex = 12;
+            // 
+            // label8
+            // 
+            this.label8.Location = new System.Drawing.Point(132, 79);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(126, 23);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "Numer Telefonu";
+            // 
+            // butFDKDodaj
+            // 
+            this.butFDKDodaj.Location = new System.Drawing.Point(373, 105);
+            this.butFDKDodaj.Name = "butFDKDodaj";
+            this.butFDKDodaj.Size = new System.Drawing.Size(116, 23);
+            this.butFDKDodaj.TabIndex = 16;
+            this.butFDKDodaj.Text = "Dodaj";
+            this.butFDKDodaj.UseVisualStyleBackColor = true;
+            // 
+            // datadoINZDataSet1
+            // 
+            this.datadoINZDataSet1.DataSetName = "DatadoINZDataSet";
+            this.datadoINZDataSet1.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // textBoxFDKNumerTelefonu
+            // 
+            this.textBoxFDKNumerTelefonu.Location = new System.Drawing.Point(132, 104);
+            this.textBoxFDKNumerTelefonu.Name = "textBoxFDKNumerTelefonu";
+            this.textBoxFDKNumerTelefonu.Size = new System.Drawing.Size(126, 20);
+            this.textBoxFDKNumerTelefonu.TabIndex = 17;
+            // 
+            // FormDodajKontrahenta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(556, 155);
+            this.Controls.Add(this.textBoxFDKNumerTelefonu);
+            this.Controls.Add(this.butFDKDodaj);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBoxFDKRegon);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBoxFDKNip);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.textBoxFDKAdresEmail);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxFDKNazwaFirmy);
+            this.Controls.Add(this.textBoxFDKNazwiskoKontahenta);
+            this.Controls.Add(this.textBoxFDKImieKontrahenta);
+            this.Name = "FormDodajKontrahenta";
+            this.Text = "Dodaj Kontahenta";
+            ((System.ComponentModel.ISupportInitialize)(this.datadoINZDataSet1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBoxFDKAdresEmail;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBoxFDKNip;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBoxFDKRegon;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxFDKNazwaFirmy;
+        private System.Windows.Forms.TextBox textBoxFDKNazwiskoKontahenta;
+        private System.Windows.Forms.TextBox textBoxFDKImieKontrahenta;
+        private System.Windows.Forms.Button butFDKDodaj;
+        private DatadoINZDataSet datadoINZDataSet1;
+        private System.Windows.Forms.TextBox textBoxFDKNumerTelefonu;
+    }
+}
--- FormDodajPrzedmiot.Designer.cs
+++ FormDodajPrzedmiot.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace AplikacjaInzynierska
 {
-    partial class FormDodajZlecenie
+    partial class FormDodajPrzedmiot
     {
         /// <summary>
         /// Required designer variable.
@@ -29,73 +29,71 @@
         private void InitializeComponent()
         {
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.comboBox2 = new System.Windows.Forms.ComboBox();
             this.SuspendLayout();
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(12, 39);
+            this.textBox1.Location = new System.Drawing.Point(13, 43);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(100, 20);
             this.textBox1.TabIndex = 0;
             // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(225, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(119, 43);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
             // label1
             // 
-            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(100, 23);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Nazwa Zlecenia";
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nazwa Przedmiotu";
             // 
             // label2
             // 
             this.label2.Location = new System.Drawing.Point(119, 13);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(123, 23);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "Osoba odpowiedzialna";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Ilość ";
             // 
             // label3
             // 
-            this.label3.Location = new System.Drawing.Point(248, 13);
+            this.label3.Location = new System.Drawing.Point(225, 13);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(123, 23);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Nazwa Firmy";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Cena";
             // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(122, 39);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(120, 21);
-            this.comboBox1.TabIndex = 4;
-            // 
-            // comboBox2
-            // 
-            this.comboBox2.FormattingEnabled = true;
-            this.comboBox2.Location = new System.Drawing.Point(251, 39);
-            this.comboBox2.Name = "comboBox2";
-            this.comboBox2.Size = new System.Drawing.Size(120, 21);
-            this.comboBox2.TabIndex = 5;
-            // 
-            // FormDodajZlecenie
+            // FormDodajPrzedmiot
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(468, 261);
-            this.Controls.Add(this.comboBox2);
-            this.Controls.Add(this.comboBox1);
+            this.ClientSize = new System.Drawing.Size(351, 99);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.textBox1);
-            this.Name = "FormDodajZlecenie";
-            this.Text = "FormDodajZlecenie";
+            this.Name = "FormDodajPrzedmiot";
+            this.Text = "Dodaj Przedmiot";
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -104,10 +102,10 @@
         #endregion
 
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.ComboBox comboBox2;
     }
 }
